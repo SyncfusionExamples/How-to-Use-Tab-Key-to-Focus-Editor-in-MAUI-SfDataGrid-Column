e30f68eab50830916be91d213b3e3885b509dd05
Muthupandy-SF4685 <muthupandy.ramakrishnan@syncfusion.com>
2026-05-14 10:48:07 +0530

    Added Demo sample and update details in ReadMe file.

---
+++ SfDataGridSample/SfDataGridSample/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Syncfusion.Maui.DataGrid;
+
+namespace SfDataGridSample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            DataGrid.CellRenderers.Remove("Template");
+            DataGrid.CellRenderers.Add("Template", new CustomDataGridTemplateRenderer());
+
+            DataGrid.CellRenderers.Remove("Text");
+            DataGrid.CellRenderers.Add("Text", new CustomDataGridTextBoxRenderer());
+
+            DataGrid.SelectionController = new CustomSelectionController(DataGrid);
+        }
+
+    }
+}
